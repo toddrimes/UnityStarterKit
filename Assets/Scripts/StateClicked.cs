c40bf82945e480d837d042f4f566e7f827b12024
Todd Rimes <todd@rimesmedia.com>
2021-02-15 19:57:50 -0800

    MVP works

--- Assets/Scripts/StateClicked.cs
+++ Assets/Scripts/StateClicked.cs
@@ -6,6 +6,7 @@ using UnityEngine.EventSystems;
 
 public class StateClicked : MonoBehaviour, IPointerClickHandler
 {
+    [SerializeField] private DataBarDisplay _dataBarDisplay;
 
     //Detect current clicks on the GameObject (the one with the script attached)
     private void Update()
@@ -21,6 +22,18 @@ public class StateClicked : MonoBehaviour, IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log($"I hit this thing: {name}");
+        string myTwoCharName = name.Substring(0, 2);
+        foreach(DataUIDisplay.StateCode state in (DataUIDisplay.StateCode[]) Enum.GetValues(typeof(DataUIDisplay.StateCode)))
+        {
+            if (state.ToString() == myTwoCharName)
+            {
+                _dataBarDisplay.stateCode1 = _dataBarDisplay.stateCode2;
+                _dataBarDisplay.stateCode2 = _dataBarDisplay.stateCode3;
+                _dataBarDisplay.stateCode3 = _dataBarDisplay.stateCode4;
+                _dataBarDisplay.stateCode4 = _dataBarDisplay.stateCode5;
+                _dataBarDisplay.stateCode5 = state;
+            }
+        }
     }
     
 }
